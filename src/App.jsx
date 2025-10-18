b3d466895537561e0464751983c317dd664e5c61
pranavtss <pranavsubburaj@gmail.com>
2025-10-19 01:09:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-5xl font-bold text-blue-400">Tailwind is Working 🚀</h1>
+      <p className="mt-4 text-lg text-gray-300">If you see this styled text, it’s working perfectly!</p>
+    </div>
+  );
+}
